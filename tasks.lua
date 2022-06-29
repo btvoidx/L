1112a54a0f46ecff73d7cc20c2e662bad0e87fa2
Maxim Tumozov <maksimtumozov@gmail.com>
2022-06-29 11:48:29 +0400

    Update tasks.lua

--- tasks.lua
+++ tasks.lua
@@ -1,19 +1,15 @@
 -- https://github.com/btvoidx/L
 
 function task.build()
-  description "Builds L cli"
+  description "Builds L cli to 'bin' directory"
   depends { task.tidy, task.test, task.lint }
-  sources { "*.go", "go.mod" }
+  sources { "**/*.go", "go.mod" }
 
   os.execute("go build -o bin ./cmd/L")
-  print("Build done, binaries are in 'bin' folder")
+  print("Build done, binaries are in 'bin' directory")
 end
 
 function task.tidy()
+  description 'Runs "go mod tidy"'
   os.execute("go mod tidy")
-end
-
-function task.default()
-  description "A default task!"
-  print("Hello world!")
 end
